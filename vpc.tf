85de53588432ca46ba390a9b08c1d6bf9d223630
21lucasmessias <21.lucasmessias@gmail.com>
2025-03-16 12:20:29 -0300

    feat: add internet gateway and route table

--- vpc.tf
+++ vpc.tf
@@ -11,7 +11,7 @@ data "aws_availability_zones" "available" {
 	state = "available"
 }
 
-resource "aws_subnet" "subnets" {
+resource "aws_subnet" "fc-subnets" {
 	count = 2
 	availability_zone = data.aws_availability_zones.available.names[count.index]
 	vpc_id = aws_vpc.fc-vpc.id
@@ -20,4 +20,30 @@ resource "aws_subnet" "subnets" {
 	tags = {
 		Name = "${var.prefix}-subnet-${count.index + 1}"
 	}
+}
+
+resource "aws_internet_gateway" "fc-igw" {
+  vpc_id = aws_vpc.fc-vpc.id
+  tags = {
+    Name = "${var.prefix}-igw"
+  }
+}
+
+resource "aws_route_table" "fc-rtb" {
+	vpc_id = aws_vpc.fc-vpc.id
+
+	route {
+		cidr_block = "0.0.0.0/0"
+		gateway_id = aws_internet_gateway.fc-igw.id
+	}
+
+	tags = {
+		Name = "${var.prefix}-rtb"
+	}
+}
+
+resource "aws_route_table_association" "fc-rtb-assoc" {
+	count = 2
+	subnet_id = aws_subnet.fc-subnets[count.index].id
+	route_table_id = aws_route_table.fc-rtb.id
 }
